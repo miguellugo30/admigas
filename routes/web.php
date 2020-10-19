45cb9c2771b5977cee9f5e21cf4764714b837f2d
miguellugo30 <48060746+miguellugo30@users.noreply.github.com>
2020-10-19 18:25:20 -0500

    paginas de inicio y registro para la web

--- routes/web.php
+++ routes/web.php
@@ -19,9 +19,17 @@ Route::get('/clientes/login', function () {
     return view('auth.login_clientes');
 })->name('login_cliente');
 
+Route::get('/clientes/login-pagina', function () {
+    return view('auth.login_clientes_pagina');
+})->name('login_cliente_pagina');
+
 Route::get('/clientes/registro/', function () {
     return view('auth.register_clientes');
 })->name('registro_cliente');
+
+Route::get('/clientes/registro-pagina/', function () {
+    return view('auth.register_clientes_pagina');
+})->name('registro_cliente_pagina');
 
 Route::post('/clientes/registro/', 'Auth\RegisterController@register')->name('registro_cliente_form');
 
