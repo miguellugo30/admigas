983be7e91914c612c3ca259ebc2678d898012442
Miguel Lugo <ingmchlugo@gmail.com>
2021-06-16 17:21:19 -0500

    Error en CRUD de mensajes

--- Modules/Configuracion/Http/Controllers/MensajesController.php
+++ Modules/Configuracion/Http/Controllers/MensajesController.php
@@ -22,7 +22,7 @@ class MensajesController extends Controller
     public function __construct()
     {
         $this->middleware(function ($request, $next) {
-            $this->empresa_id = Auth::user()->admigas_empresas_id;
+            $this->empresa_id = Auth::user()->Empresas->first()->id;
 
             return $next($request);
         });
